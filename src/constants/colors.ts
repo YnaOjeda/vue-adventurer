36fd637d8660def1ab3ef10468b75d77c085684f
YnaOjeda <yna.ojeda@gmail.com>
2024-10-06 21:53:10 +1300

    feat: add color definitions for different face parts

---
+++ src/constants/colors.ts
@@ -0,0 +1,43 @@
+export const SkinColor = {
+  'skin-tone-1': '#321B0F',
+  'skin-tone-2': '#70361C',
+  'skin-tone-3': '#8D5524',
+  'skin-tone-4': '#C68642',
+  'skin-tone-5': '#E0AC69',
+  'skin-tone-6': '#F1C27D',
+  'skin-tone-7': '#FFDBAC',
+};
+
+export const EyeColor = {
+  'eye-grey-1': '#A9A9A9',
+  'eye-brown-1': '#4B3D3D',
+  'eye-brown-2': '#7B4B3A',
+  'eye-brown-3': '#A65E2E',
+  'eye-hazel-1': '#D9A34D',
+  'eye-green-1': '#4B9B3F',
+  'eye-green-2': '#A8D68D',
+  'eye-blue-1': '#1E90FF',
+  'eye-blue-2': '#ADD8E6',
+};
+
+export const HairColor = {
+  'hair-black-1': '#101010',
+  'hair-grey-1': '#7D7D7D',
+  'hair-red-1': '#A52A2A',
+  'hair-brown-1': '#8B4513',
+  'hair-blond-1': '#F4A460',
+  'hair-blond-2': '#F3E5AB',
+  'hair-blue-1': '#1E90FF',
+  'hair-green-1': '#4CAF50',
+  'hair-purple-1': '#A365DB',
+  'hair-pink-1': '#FFB6C1',
+};
+
+export const EyeGlassesColor = {
+  'glasses-black-1': '#101010',
+  'glasses-grey-1': '#C0C0C0',
+  'glasses-red-1': '#FF0000',
+  'glasses-yellow-1': '#FFD700',
+  'glasses-blue-1': '#3B5998',
+  'glasses-green-1': '#4CAF50',
+};
