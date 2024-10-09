614f8ad2a820468637237ddc097712df299ffbcd
YnaOjeda <yna.ojeda@gmail.com>
2024-10-09 23:07:04 +1300

    fix: remove unnecessary logic for viewBox of face

--- src/components/FeatureGenerator.vue
+++ src/components/FeatureGenerator.vue
@@ -2,7 +2,7 @@
   <svg
     :height="size"
     :width="size"
-    :viewBox="featureKey === FeatureKey.face ? '0 0 762 762' : '0 0 1024 1024'"
+    viewBox="0 0 1024 1024"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     v-html="variantHTML"
@@ -11,7 +11,7 @@
 
 <script lang="ts" setup>
 import { computed } from 'vue';
-import { FeatureKey, FeatureProps } from '../constants/features';
+import { FeatureProps } from '../constants/features';
 import { getSVGContent } from '../utils';
 
 const props = defineProps<FeatureProps>();
