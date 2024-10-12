0fe71ff772df525d9186af020e083e029493455e
YnaOjeda <yna.ojeda@gmail.com>
2024-10-13 01:52:24 +1300

    test: add tests for getSVGContent

---
+++ src/tests/unit/getSVGContent.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+import { getSVGContent, isValidColor } from '../../utils';
+import {
+  ColorFlag,
+  FeatureInfo,
+  FeatureKeys,
+  FeatureType,
+} from '../../constants/features';
+
+// as FeatureType used to avoid linting errors
+describe('getSVGContent', () => {
+  it('should return an empty string for an invalid featureKey', () => {
+    expect(getSVGContent('invalidFeature' as FeatureType)).toBe('');
+  });
+
+  it('should return the default variant content if variantKey is invalid', () => {
+    // This also ensures that default variants are defined in all Feature Objects
+    FeatureKeys.forEach((key) => {
+      expect(getSVGContent(key as FeatureType, 'invalidVariantKey')).not.toBe(
+        ''
+      );
+    });
+  });
+
+  it('should always be able to replace ColroFlag with a valid value', () => {
+    let featureKeyWithFlag = '' as FeatureType;
+    Object.entries(FeatureInfo).forEach(([key, feature]) => {
+      if (!featureKeyWithFlag && feature.defaultColor) {
+        // this key will be used to check the replace-logic later
+        featureKeyWithFlag = key as FeatureType;
+      }
+      // Ensure that all default values are valid colors
+      if (feature.defaultColor) {
+        expect(isValidColor(feature.defaultColor)).toBe(true);
+      }
+
+      // Ensure that all features with the color flag have default colors
+      const hasColorFlag = Object.values(feature.variants).some((variant) =>
+        variant.includes(ColorFlag)
+      );
+      if (hasColorFlag) {
+        expect(feature.defaultColor).toBeTruthy();
+      }
+    });
+
+    // Finally, the color flag should be removed from the return value
+    expect(getSVGContent(featureKeyWithFlag)).not.toContain(ColorFlag);
+  });
+});
