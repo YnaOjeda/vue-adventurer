93681f160fdabe0ddc92d5f0bfa5f4ba50c1e203
YnaOjeda <yna.ojeda@gmail.com>
2024-10-20 22:12:21 +1300

    build: update vite and tsconfig configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,18 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [
+    vue(),
+    dts({
+      rollupTypes: true,
+      insertTypesEntry: true,
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'VueAdventurer',
+      name: 'vue-adventurer',
       fileName: 'vue-adventurer',
     },
     rollupOptions: {
